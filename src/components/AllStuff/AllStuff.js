857485f47e9b6ce2b70ca91ddde3492bf5c41057
Alex Orga <alexorga.dev@gmail.com>
2018-07-11 19:34:05 -0500

    pages are displaying and routes are set, json has been imported to the database

---
+++ src/components/AllStuff/AllStuff.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './AllStuff.scss';
+
+class AllStuff extends React.Component {
+  render () {
+    return (
+      <h1>All Stuff</h1>
+    );
+  }
+}
+
+export default AllStuff;
